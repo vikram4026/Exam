ea8dc49350392166d4dba954f11ca42371e68809
vikram4026 <gmvikramgmv@gmail.com>
2022-06-14 13:40:15 +0530

    Deepak has finish code

--- src/com/tsesyt/Thamous.java
+++ src/com/tsesyt/Thamous.java
@@ -2,6 +2,11 @@ package com.tsesyt;
 
 public class Thamous {
 	
+	private void dee() {
+
+		System.out.println("Iam Deepak...");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("This Iron man");
