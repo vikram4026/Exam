2204b7d31c78d566e65dc3470a59701d39213ce8
vikram4026 <gmvikramgmv@gmail.com>
2022-06-14 13:49:28 +0530

    This Vinoth Kumar

--- src/com/tsesyt/Thamous.java
+++ src/com/tsesyt/Thamous.java
@@ -2,6 +2,11 @@ package com.tsesyt;
 
 public class Thamous {
 	
+	private void vinothKumar() {
+
+		System.out.println("Vinoth Kumar has pused");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("This Iron man");
